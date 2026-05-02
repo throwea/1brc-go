8bbc06266ded7346dabd05688f93b97bc1d0c464
throwea <saeruig@saeruig.localdomain>
2026-05-02 00:33:13 -0500

    truncate numbers

--- my-implementation/documentation/journey.md
+++ my-implementation/documentation/journey.md
@@ -1,3 +1,4 @@
 ## First day
 - I pretty quickly implemented the logic for parsing the file
 - For just reading in the file line by line, it takes 27.68 seconds
+- Okay, now, I'm trying to convert each line into a measurement object. (5-2-oom.txt
--- my-implementation/main.go
+++ my-implementation/main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
+
+type measurement struct {
+	city string
+	temp float64
+}
 
 // TODO:
 // - Read in the file line by line -> DONE
@@ -29,13 +37,34 @@ func parseFile() {
 	fileScanner := bufio.NewScanner(readFile)
 	fileScanner.Split(bufio.ScanLines)
 
-	// lines := 1000
+	lines := 1000
+	data := make([]measurement, 0, 1000)
 	for fileScanner.Scan() {
-		// fmt.Println(fileScanner.Text())
-		// lines -= 1
-		// if lines <= 0 {
-		// 	break
-		// }
+		text := fileScanner.Text()
+		measurement := processLine(text)
+		fmt.Printf("%v\n", measurement)
+		data = append(data, measurement)
+		lines -= 1
+		if lines <= 0 {
+			break
+		}
 	}
 	readFile.Close()
+}
+
+func processLine(text string) measurement {
+	split := strings.Split(text, ";")
+	dig, err := strconv.ParseFloat(split[1], 64)
+	if err != nil {
+		panic(err)
+	}
+	temp := truncateNaive(dig, 0.1) // No good. We don't need this much precision
+	return measurement{
+		city: split[0],
+		temp: temp,
+	}
+}
+
+func truncateNaive(f float64, unit float64) float64 {
+	return math.Trunc(f/unit) * unit
 }
